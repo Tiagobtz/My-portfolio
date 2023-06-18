93b4a9a03778754a360bbb1de7fb3be5705c8480
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-06-17 22:56:03 -0300

    Atualizando

--- src/js/interação.js
+++
@@ -1,8 +0,0 @@
-const botao = document.querySelector("#botao-sobre-mim")
-const apresentacao = document.querySelector("#apresentacao")
-
-botao.addEventListener("click", function(){
-    apresentacao.classList.add("ativo")
-    skills.classList.add("ativo")
-    botao.classList.add("desativado")
-})
